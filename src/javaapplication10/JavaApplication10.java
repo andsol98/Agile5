1e35d01db0a0a830311086f4bb7a82d75763cc26
Chris <crs0054@mix.wvu.edu>
2019-03-19 19:03:05 -0400

    I made thing uninvisible

--- src/javaapplication10/JavaApplication10.java
+++ src/javaapplication10/JavaApplication10.java
@@ -17,6 +17,7 @@ public class JavaApplication10 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        new NewJFrame().setVisible(true);
     }
     
 }
